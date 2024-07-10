b384370f8af76b366ce468bb54ce03f0fb959b38
Dzmitry Lahunouski <killroy192@gmail.com>
2024-07-10 10:04:07 +0100

    fix report timing

--- src/cli/report.ts
+++ src/cli/report.ts
@@ -3,11 +3,16 @@ import { reportExecutors } from "src/packages/reports";
 
 (async function main() {
   const reportType: keyof typeof reportExecutors = await select({
-    message: "Select a package manager",
+    message: "Select a report type",
     choices: [
       {
         name: "scroll",
         value: "scroll",
+      },
+      {
+        name: "fuel",
+        value: "symbolic",
+        disabled: true,
       },
       {
         name: "symbolic",
