c7b7840d21149e1dc0819bff8339c88a807d7c35
Dzmitry Lahunouski <killroy192@gmail.com>
2024-07-17 21:42:10 +0100

    make public config

--- src/libs/configs.ts
+++ src/libs/configs.ts
@@ -2,6 +2,7 @@ import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { decryptJson } from "./crypt";
 import { getMasterKey } from "./shared";
+import { JsonObj } from "src/types/common";
 import { AppConfig, Profile } from "src/types/configs";
 
 let config: AppConfig;
@@ -9,7 +10,10 @@ let config: AppConfig;
 export const getAppConf = async () => {
   if (!config) {
     const masterKey = await getMasterKey();
-    config = decryptJson(readFileSync(resolve(".", ".app.config"), "utf-8"), masterKey) as AppConfig;
+    config = {
+      ...(decryptJson(readFileSync(resolve(".", ".app.secrets"), "utf-8"), masterKey) as JsonObj),
+      ...(JSON.parse(readFileSync(resolve(".", "app.config.json"), "utf-8")) as JsonObj),
+    } as AppConfig;
   }
   return config;
 };
